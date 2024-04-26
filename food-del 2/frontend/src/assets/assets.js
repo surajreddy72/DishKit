197fd1b79cf6ecc13fe2a94f147a0088a5c7ba6e
pavansaipendry <74443843+pavansaipendry@users.noreply.github.com>
2024-04-27 00:55:28 +0530

    BIryani section added under menu_9.
    Here dowlnoad any biryani image and place it in the assests and save it as menu_9. Run again to see the changes. and change the code in the line 13 after modifying.

--- food-del 2/frontend/src/assets/assets.js
+++ food-del 2/frontend/src/assets/assets.js
@@ -10,6 +10,7 @@ import menu_5 from './menu_5.png'
 import menu_6 from './menu_6.png'
 import menu_7 from './menu_7.png'
 import menu_8 from './menu_8.png'
+import menu_9 from './menu_9.avif'
 
 import food_1 from './food_1.png'
 import food_2 from './food_2.png'
@@ -114,7 +115,13 @@ export const menu_list = [
     {
         menu_name: "Noodles",
         menu_image: menu_8
-    }]
+    },
+    {
+        menu_name: "Biryani",
+        menu_image: menu_9
+    }
+    
+]
 
 export const food_list = [
     {
