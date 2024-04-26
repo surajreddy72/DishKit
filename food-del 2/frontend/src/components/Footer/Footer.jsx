c9f54e0d33fc5a63c975f4a5861d8fb3247857ef
pavansaipendry <74443843+pavansaipendry@users.noreply.github.com>
2024-04-27 00:48:35 +0530

    Changes in Footer

--- food-del 2/frontend/src/components/Footer/Footer.jsx
+++ food-del 2/frontend/src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="footer-content">
         <div className="footer-content-left">
             <img src={assets.logo} alt="" />
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            <p>Disk Kit is an online meal kit delivery service that provides the ingredients for you to prepare your favorite healthy meals at home.</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
                 <img src={assets.twitter_icon} alt="" />
@@ -27,13 +27,13 @@ const Footer = () => {
         <div className="footer-content-right">
             <h2>GET IN TOUCH</h2>
             <ul>
-                <li>+1-212-456-7890</li>
-                <li>contact@tomato.com</li>
+                <li>+91 9182931340</li>
+                <li>dishkit@gmail.com</li>
             </ul>
         </div>
       </div>
       <hr />
-      <p className="footer-copyright">Copyright 2024 © Tomato.com - All Right Reserved.</p>
+    
     </div>
   )
 }
